778dc439309ac0347a2aa8aa618b14deee390c31
furkan-uzmez <furkan.n41@hotmail.com>
2026-02-15 23:34:14 +0300

    fix logging

--- batchsize/functions/logger.py
+++ batchsize/functions/logger.py
@@ -6,19 +6,23 @@ def get_logger(log_path='training.log'):
     logger = logging.getLogger('training_logger')
     logger.setLevel(logging.INFO)
 
-    # Aynı logger birden fazla handler eklenmesini önlemek için:
-    if not logger.handlers:
-        # Formatlayıcı
-        formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt='%Y_%m_%d_%H_%M_%S')
+    # Her çağrıda yeni bir dosya handler'ı ekleyebilmek için mevcut handler'ları temizleyelim:
+    if logger.handlers:
+        for handler in logger.handlers[:]:
+            logger.removeHandler(handler)
+            handler.close()
 
-        # Dosya yazıcı
-        file_handler = logging.FileHandler(log_path)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+    # Formatlayıcı
+    formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt='%Y_%m_%d_%H_%M_%S')
 
-        # Konsola yazıcı
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
+    # Dosya yazıcı
+    file_handler = logging.FileHandler(log_path, mode='a', encoding='utf-8')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # Konsola yazıcı
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
     return logger
